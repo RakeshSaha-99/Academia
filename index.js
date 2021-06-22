0a0ef54a2aff1b4326a85c17704b34a565cca7f2
RakeshSaha-99 <64775346+RakeshSaha-99@users.noreply.github.com>
2021-06-23 03:32:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function registerLogout(){
         window.location.reload();
     } 
     else {
-        window.location.href = "/Academia/blob/main/register.html";
+        window.location.href = "Academia/blob/main/register.html";
     }
 }
